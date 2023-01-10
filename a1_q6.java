b5f20bfd457eb92b47936f6b35c5c4511d71bc5e
harsheenkohli <122385400+harsheenkohli@users.noreply.github.com>
2023-01-10 23:26:53 +0530

    Submitting Q6 of Assignment 1.

---
+++ a1_q6.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class a1_q6
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("Enter a number to reverse : ");
+        int n = sc.nextInt();
+        int temp = n;
+        while(temp!=0)
+        {
+            System.out.println(temp%10);
+            temp/=10;
+        }  
+        if (n == 0) 
+            System.out.println(n);  
+    }
+}
