24cfee3b908fa7cc6fabc980996f59f426be0d34
harsheenkohli <122385400+harsheenkohli@users.noreply.github.com>
2023-01-10 23:06:36 +0530

    Submitting Q3 of Assignment 1.

---
+++ a1_q3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class a1_q3
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("Enter the quantity of numbers to be checked : ");
+        int t = sc.nextInt();
+        for (int i = 1; i<=t; i++)
+        {
+            System.out.println("Enter a number to check for prime or not:");
+            int n = sc.nextInt();
+            if (n<0)
+            {
+                System.out.println("Invalid input.");
+                continue;
+            }
+            int counter = 0;
+            for (int j = 1; j<=n; j++)
+            {
+                if(n%j==0)
+                    counter++;
+            }
+            if (counter == 1 || counter == 0)
+                System.out.println("Neither prime nor composite.");
+            else if (counter == 2)
+                System.out.println("Prime.");
+            else
+                System.out.println("Not prime (composite).");
+        }
+    }
+}
