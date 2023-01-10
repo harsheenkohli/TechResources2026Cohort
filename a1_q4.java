9725c658195f761e13f3e4ea31ccfbd842dc1b1c
harsheenkohli <122385400+harsheenkohli@users.noreply.github.com>
2023-01-10 23:14:24 +0530

    Submitting Q4 of Assignment 1.

---
+++ a1_q4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class a1_q4
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("Enter the lower limit : ");
+        int low = sc.nextInt();
+        System.out.println ("Enter the upper limit : ");
+        int high = sc.nextInt();
+        System.out.println("Prime numbers from "+ low +" to "+ high +" are :");
+        for (int i=low; i<=high; i++)
+        {
+            int counter = 1;
+            for (int j = 2; j<=i; j++)
+            {
+                if (i%j==0)
+                    counter++;
+                if (counter >2)
+                    break;   
+            }
+            if (counter==2)
+                System.out.println(i);
+        }
+    }
+}
