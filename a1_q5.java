bafe71bea49a478b2687812c0029b36a65b27b11
harsheenkohli <122385400+harsheenkohli@users.noreply.github.com>
2023-01-10 23:23:14 +0530

    Submitting Q5 of Assignment 1.

---
+++ a1_q5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class a1_q5
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("Enter a number to check for number of digits : ");
+        int n = sc.nextInt();
+        int temp = n;
+        int counter = 0;
+        while (temp!=0)
+        {
+            counter++;
+            temp/=10;
+        } 
+        if (counter==0)
+            System.out.println("The number of digits in "+ n + " are 1."); 
+        else
+            System.out.println("The number of digits in "+ n + " are "+ counter+ ".");     
+    }
+}
