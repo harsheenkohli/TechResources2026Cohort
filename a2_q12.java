0891501ca025279abbd2aa1cae093a0262a6e642
Harsheen Kaur Kohli <122385400+harsheenkohli@users.noreply.github.com>
2023-01-15 17:24:18 +0530

    Submitting Q12 of Assignment 2.

--- a2_q12.java
+++ a2_q12.java
@@ -5,7 +5,7 @@ public class a2_q12
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of fibonaccci pattern:");
-        int n = sc.nextInt(); //7
+        int n = sc.nextInt(); 
         int first = 0, second = 1, third = first + second;
         System.out.println(first);
         for (int i = 2; i<=n; i++)
